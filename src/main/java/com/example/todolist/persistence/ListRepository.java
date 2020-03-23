c58cffab435f90a370f9c3b6e9b23a182bed88f9
prugh <ruggirello99@hotmail.it>
2020-03-23 13:55:31 +0100

    fixed test

--- src/main/java/com/example/todolist/persistence/ListRepository.java
+++ src/main/java/com/example/todolist/persistence/ListRepository.java
@@ -4,5 +4,5 @@ import com.example.todolist.model.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ListRepository extends JpaRepository<TaskList, Long> {
-    public TaskList findByNome(String nome);
+    TaskList findByNome(String nome);
 }
--- src/test/java/com/example/todolist/TodolistApplicationTests.java
+++ src/test/java/com/example/todolist/TodolistApplicationTests.java
@@ -2,18 +2,27 @@ package com.example.todolist;
 
 import com.example.todolist.model.Task;
 import com.example.todolist.model.TaskList;
+import com.example.todolist.persistence.ListRepository;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodolistApplicationTests {
+
+    @Autowired
+    private ListRepository listRepository;
 
     @Test
     public void contextLoads() {
@@ -25,19 +34,29 @@ public class TodolistApplicationTests {
 
     @Test
     public void createTaskList() {
+        System.out.println("NOME : " + randomTaskList.getNome());
         Response response = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .post(API_ROOT + "list/create?nome=" + randomTaskList.getNome());
 
+        response.getBody().prettyPrint();
         assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    }
+
+    public void createTaskList(TaskList list) {
+        System.out.println("NOME LISTA: " + list.getNome());
+        Response response = RestAssured.given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .post(API_ROOT + "list/create?nome=" + list.getNome());
     }
 
     @Test
     public void createTask() {
         Task task = createRandomTask();
+        createTaskList(randomTaskList);
         Response response = RestAssured.given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .post(API_ROOT + "task/create?nome=" + task.getNome() + "&idList=" + randomTaskList.getId());
+                .post(API_ROOT + "task/create?nome=" + task.getNome() + "&idList=" + listRepository.findByNome(randomTaskList.getNome()).getId());
 
         assertEquals(HttpStatus.OK.value(), response.getStatusCode());
     }
